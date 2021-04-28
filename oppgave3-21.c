1057375181bf713934c1d526cbeb3a5f4ba33e04
TeacupMonkey <79581649+TeacupMonkey@users.noreply.github.com>
2021-04-28 12:17:10 +0200

    Create oppgave3-21.c

---
+++ oppgave3-21.c
@@ -0,0 +1,43 @@
+#include<stdio.h>
+#include<stdlib.h>
+#include<sys/wait.h>
+#include<unistd.h>
+
+
+int main(int argc, char **argv){
+   int num;
+  
+   //Sjekker om det har kommet argument i kommandolinjen - og gir beskjed
+   if(argc==1){
+        printf("\nNo extra command line argument passed other than program name.\n\n");
+        exit(0);
+    }
+  
+
+    // Konverterer char til integer
+    num = atoi(argv[1]);
+  
+    //Sjekker om argumentet er et positivt heltall eller ikke
+    if(num <= 0){
+       printf("\nThe number should be a positive interger.\n\n");
+        exit(0);
+    }
+  
+    if(fork() == 0){
+       //Child prosessen
+       while(num > 1){
+           printf("%d ", num);
+           if(num %2 == 0)
+               num = num / 2;
+           else
+               num = 3 * num + 1;      
+       }
+       printf("1\n\n");
+    }
+    else{
+       //Parent prosessen - Venter for at child prosessen skal avslutte
+       wait(NULL);
+    }
+  
+    return 0;
+}
