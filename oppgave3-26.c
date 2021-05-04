613990c23bc1382a64f7c509ffb8e54d49505bbd
TeacupMonkey <79581649+TeacupMonkey@users.noreply.github.com>
2021-05-04 13:16:15 +0200

    Create oppgave3-26.c

---
+++ oppgave3-26.c
@@ -0,0 +1,48 @@
+#include <fcntl.h>
+#include <stdio.h>
+#include <unistd.h>
+
+// Metode for å bytte uppercase til lowercase og motsatt. 
+//Denne metoden vil loope gjennom en array og bytte størrelsen på chars, dette forgår i child prosessen.
+void switchCases(char* message) {
+    int i = 0;
+    while(message[i] != '\0') {
+        if(message[i] > 64 && message[i] < 91) {
+            message[i]+=32;
+        }
+        else if(message[i] > 96 && message[i] < 123) {
+            message[i]-=32;
+        }
+        i++;
+    }
+}
+ 
+//Main metoden begynner med å lage to pipes, så vil den lage en fork.
+//Child prosessen vil lukke pipen og lese meldingen og skrive det til pipen. 
+//Så vil parent prosessen lukke pipen og lese den og gi et output av svaret.
+int main(int argc, char** argv){
+    int fd[2];
+    char message[10] = "Hi there!";
+    if (pipe(fd) == -1) {
+        printf("There was an error in pipe creation. Program will now exit.\n");
+        return -1;
+    }
+    pid_t child;
+    child = fork();
+    if(child < 0) {
+        printf("Forking failed.");
+        return -1;
+    }
+    if(child == 0) {
+        close(fd[0]);
+        switchCases(message);
+        write(fd[1],message,10);
+    }
+    else if(child > 0) {
+        close(fd[1]);
+        char read_buffer[10];
+        read(fd[0],read_buffer,10);
+        printf("%s",read_buffer);
+    }
+    return 0;
+}
