f731193d0de1eb8459c686133b8d5788362ff8f7
TeacupMonkey <79581649+TeacupMonkey@users.noreply.github.com>
2021-05-16 14:54:48 +0200

    Update oppgave3-2.c

--- kildekode/oppgave3-2.c
+++ kildekode/oppgave3-2.c
@@ -1,12 +1,27 @@
+/*
+* Modul 3
+* Oppgave 3.2
+*
+* Med denne koden vil vi se hva som skjer når vi utløser 3 fork() systemkall etter hverandre,
+* vi er nysgjerrige på hvor mange parent og child prosesser vi får til sammen.
+*/
+
+// Importerer fra biblioteket
 #include <stdio.h>
 #include <unistd.h>
 
 int main() {
-  fork();
+  fork(); // kaller første fork()
   
-  fork();
+  fork(); // kaller andre fork()
   
-  fork();
+  fork(); // kaller tredje fork()
+  
+  /*
+  * Her kunne vi også hatt et output for å se hvor mange prosesser som
+  * kjører men siden dette er et eksempel fra pensumsboka valgte vi
+  * ikke å ta det med.
+  */
   
   return 0;
 }
